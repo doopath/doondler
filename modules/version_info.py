735a06eef4a4ec2f618737f2a8a7e5dd48e53f68
doopath <maminpahat@gmail.com>
2021-03-07 11:16:21 +0300

    version info updated

--- modules/version_info.py
+++ modules/version_info.py
@@ -18,7 +18,7 @@
 
 info = {
     "build_number": "UCB-06032021-02",
-    "version": "0.5.1",
+    "version": "0.5.11",
     "kernel": "linux 5.11.2",
     "platform": "AMD Ryzen 3600U",
     "os": "Ubuntu-20.04"
